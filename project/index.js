fe27767bc7f3b4877f10c295a463eeb854dd5d33
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2026-01-14 15:42:34 +0200

    Add exercise 1.5

--- project/index.js
+++ project/index.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Server started in port ${PORT}`)
