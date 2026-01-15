0fc7f08be00518c5cd918190b39139db24614d8b
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2026-01-15 12:28:37 +0200

    Refactor log_output and make it a server

--- log_output/index.js
+++ log_output/index.js
@@ -1,9 +1,24 @@
-const logger = () => {
+const express = require("express")
+
+let currentHashLine = ""
+
+const updateHashLine = () => {
   const currentDate = new Date().toISOString()
   const hash = crypto.randomUUID()
-
-  console.log(`${currentDate}: ${hash}`)
-  setTimeout(() => logger(), 5000)
+  currentHashLine = `${currentDate}: ${hash}`
+  console.log(currentHashLine)
 }
 
-logger()
+updateHashLine()
+setInterval(updateHashLine, 5000)
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get("/", (request, response) => {
+  response.type("text/plain").send(currentHashLine)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
