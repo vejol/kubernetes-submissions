e74a4a0323ebff192c91018bf632e9d02a249d3e
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2026-01-31 19:59:04 +0200

    Create Broadcaster

---
+++ chapter5/project/broadcaster/index.js
@@ -0,0 +1,27 @@
+import "dotenv/config"
+import axios from "axios"
+import { connect, StringCodec } from "nats"
+
+const FULLSTACK_DISCORD_WEBHOOK = process.env.FULLSTACK_DISCORD_WEBHOOK
+const NATS_URL = process.env.NATS_URL
+
+const sc = StringCodec()
+const nc = await connect({ servers: NATS_URL })
+
+console.log(`Connected to NATS: ${nc.getServer()}`)
+
+const subjects = ["todo_created", "todo_updated"]
+
+for (const subject of subjects) {
+  nc.subscribe(subject, {
+    callback: (err, msg) => {
+      const payloadText = sc.decode(msg.data)
+
+      const message = {
+        content: `${subject}:\n\n${payloadText}`,
+      }
+
+      axios.post(FULLSTACK_DISCORD_WEBHOOK, message)
+    },
+  })
+}
